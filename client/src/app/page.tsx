e0b28978870deb6dcb9252b1a14f5fd3ccd87caf
SauceCode01 <sauce.code.01@gmail.com>
2025-12-29 02:12:27 +0800

    hotfix: deployment error

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,13 +1,9 @@
 import Link from "next/link";
-import React from "react";
 
 const page = async () => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000"}/api/test`);
-  const data = await res.json();
   return (
     <>
       <div>Home page</div>
-      <div>{JSON.stringify(data)}</div>
       <Link href="/policies">Go to Policies page</Link>
     </>
   );
